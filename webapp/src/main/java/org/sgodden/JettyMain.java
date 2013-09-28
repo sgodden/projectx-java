e22eb7f93dd0649f7698209868fc2f7223a88110
Simon Godden <simon@blackcatsolutions.co.uk>
2013-09-28 15:52:12 +0100

    Initial angular and bootstrap

--- webapp/src/main/java/org/sgodden/JettyMain.java
+++ webapp/src/main/java/org/sgodden/JettyMain.java
@@ -1,7 +1,9 @@
 package org.sgodden;
 
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.resource.FileResource;
@@ -21,16 +23,18 @@ public class JettyMain {
             context.setContextPath("/");
             System.out.println(new FileResource(new File("src/main/webapp").toURI().toURL()));
             context.setBaseResource(new FileResource(new File("src/main/webapp").toURI().toURL()));
-            server.setHandler(context);
-
             context.setInitParameter("contextConfigLocation", "WEB-INF/beans.xml");
             context.addEventListener(new ContextLoaderListener());
 
             context.addServlet(CXFServlet.class, "/services/*");
 
-//            ResourceHandler handler = new ResourceHandler();
-//            handler.setBaseResource(new FileResource(new File("src/main/webapp").toURI().toURL()));
-//            server.setHandler(handler);
+            ResourceHandler handler = new ResourceHandler();
+            handler.setBaseResource(new FileResource(new File("src/main/webapp").toURI().toURL()));
+
+            HandlerList handlerList = new HandlerList();
+            handlerList.setHandlers(new Handler[]{context, handler});
+
+            server.setHandler(handlerList);
 
             server.start();
             server.join();
