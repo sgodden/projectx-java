b208be6996bf28edf4f294137d93b5d69e15c007
Simon Godden <simon@blackcatsolutions.co.uk>
2013-09-29 16:59:30 +0100

    A working unit test for orders controller

--- webapp/src/main/webapp/angular/test/unit/controllersSpec.js
+++ webapp/src/main/webapp/angular/test/unit/controllersSpec.js
@@ -1,16 +1,28 @@
-'use strict';
+describe('controllers', function () {
+	beforeEach(module('myApp.controllers'));
 
-/* jasmine specs for controllers go here */
+	describe("CustomerOrdersController", function() {
+		var scope;
+		beforeEach(inject(function($rootScope, $controller) {
+			scope = $rootScope.$new(),
+				mockService = {
+					query: function() {
+						return [
+							{orderNumber: 'O001'},
+							{orderNumber: 'O001'}
+						];
+					}
+				};
+			ctrl = $controller('CustomerOrdersController', {
+				$scope: scope,
+				CustomerOrdersService: mockService
+			});
+		}));
 
-describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+		it("should create orders model with 2 orders fetched from server", function() {
+			expect(scope.orders.length).toBe(2);
+			expect(scope.orders[0].orderNumber).toBe('O001');
+		});
 
-
-  it('should ....', inject(function() {
-    //spec body
-  }));
-
-  it('should ....', inject(function() {
-    //spec body
-  }));
+	});
 });
