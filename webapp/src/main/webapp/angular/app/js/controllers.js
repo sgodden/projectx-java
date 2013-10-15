567476ea3530e8abbbf3357181b4c17061977c9c
Simon Godden <simon@blackcatsolutions.co.uk>
2013-10-15 19:54:56 +0100

    half-assed working date picker

--- webapp/src/main/webapp/angular/app/js/controllers.js
+++ webapp/src/main/webapp/angular/app/js/controllers.js
@@ -1,16 +1,19 @@
 'use strict';
 
-angular.module('myApp.controllers', ['$strap.directives'])
+angular.module('myApp.controllers', ['ui.date'])
 	.controller('CustomerOrdersController', function ($scope, CustomerOrdersService) {
 		$scope.orders = CustomerOrdersService.query();
 	})
 	.controller('CustomerOrderController', function ($scope, $routeParams, CustomerOrdersService) {
-		var order = CustomerOrdersService.get({id: $routeParams.id});
-		order.bookingDate = new Date(order.bookingDate);
+		var order = CustomerOrdersService.get({id: $routeParams.id}, function(order) {
+			// TODO - needs to be done on a transformation of the service
+			order.bookingDate = new Date(order.bookingDate);
+			console.log(order.bookingDate);
+		});
 		$scope.order = order;
 
 		$scope.submit = function() {
-			console.log($scope.order.orderNumber);
+			console.log($scope.order.bookingDate);
 			$scope.order.$save(function() {
 				window.location.href = '#/orders';
 			});
