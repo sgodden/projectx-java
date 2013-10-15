90edf05420102865bcd1746b103e7e54178e3248
Simon Godden <simon@blackcatsolutions.co.uk>
2013-10-15 20:30:13 +0100

    Encapsulate transformation of dates

--- webapp/src/main/webapp/angular/app/js/controllers.js
+++ webapp/src/main/webapp/angular/app/js/controllers.js
@@ -2,18 +2,17 @@
 
 angular.module('myApp.controllers', ['ui.date'])
 	.controller('CustomerOrdersController', function ($scope, CustomerOrdersService) {
-		$scope.orders = CustomerOrdersService.query();
+		CustomerOrdersService.query().then(function(response) {
+			$scope.orders = response.data;
+		});
 	})
 	.controller('CustomerOrderController', function ($scope, $routeParams, CustomerOrdersService) {
-		var order = CustomerOrdersService.get({id: $routeParams.id}, function(order) {
-			// TODO - needs to be done on a transformation of the service
-			order.bookingDate = new Date(order.bookingDate);
-			console.log(order.bookingDate);
+		$scope.order = {};
+		CustomerOrdersService.get({id: $routeParams.id}).then(function(response) {
+			$scope.order = response.data;
 		});
-		$scope.order = order;
 
 		$scope.submit = function() {
-			console.log($scope.order.bookingDate);
 			$scope.order.$save(function() {
 				window.location.href = '#/orders';
 			});
--- webapp/src/main/webapp/angular/app/js/services.js
+++ webapp/src/main/webapp/angular/app/js/services.js
@@ -5,8 +5,26 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', ['ngResource']).
-	factory('CustomerOrdersService',function ($resource) {
-		return $resource('/services/myservice/:id');
+angular.module('myApp.services', []).
+	factory('CustomerOrdersService',function ($http) {
+		return {
+			query: function(params) {
+				return $http({
+					method: "GET",
+					url: "/services/myservice/"
+				});
+			},
+			get: function(params) {
+				return $http({
+					method: "GET",
+					url: "/services/myservice/" + params.id,
+					transformResponse: function(response) {
+						var order = JSON.parse(response);
+						order.bookingDate = new Date(order.bookingDate);
+						return order;
+					}
+				});
+			}
+		}
 	}).
 	value('version', '0.1');
